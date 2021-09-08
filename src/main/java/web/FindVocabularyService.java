e5f11228bd07cf713764faf846887888fa4e1768
Gordei <gordei_vihlyaev@mail.ru>
2021-09-08 22:48:45 +0500

    Singelton, PostConstruct

--- src/main/java/web/FindVocabularyService.java
+++ src/main/java/web/FindVocabularyService.java
@@ -1,19 +1,40 @@
 package web;
 
+import dao.DaoException;
+import dao.VocabularyDao;
 import entity.Vocabulary;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import util.PoolConnectionManager;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.inject.Singleton;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 
 @Path("/find")
+@Singleton
 public class FindVocabularyService {
+    private static final Logger logger = LoggerFactory.getLogger(FindVocabularyService.class);
+    private VocabularyDao vocabularyDao;
+
+    @PostConstruct
+    public void init() {
+        logger.info("Servlet is created");
+        vocabularyDao = new VocabularyDao();
+        vocabularyDao.setConnectionManager(new PoolConnectionManager());
+    }
+
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Vocabulary findVocabulary(Vocabulary vocabulary) {
-        System.out.println(vocabulary.toString());
-        return new Vocabulary("good request");
+    public Vocabulary findVocabulary(Vocabulary vocabulary) throws DaoException {
+        logger.info(vocabulary.toString());
+        List<Vocabulary> resultVocabulary = vocabularyDao.findVocabulary(vocabulary.getName());
+        return resultVocabulary.get(0);
     }
 }
 
